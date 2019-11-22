12d3d47f02dff9aeceed788b052212322fefb3ac
Scotty Fulton <sfulton@talentpath.com>
2019-11-21 19:01:20 -0600

    another one

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import LoginWindow from "./LoginWindow";
 import RenderWindow from "./RenderWindow";
+import Nav from "../components/NavigationBar";
 import "../styleSheets/App.css";
 import {
     BrowserRouter as Router,
@@ -16,10 +17,14 @@ class App extends React.Component {
         this.state = {
             isLoggedIn: false,
             showLink: true,
-            whichMessage: true
+            whichMessage: true,
+            showAllNav: false
         };
     }
 
+    handleAllNav = event => {
+        this.setState({ showAllNav: event });
+    };
     handleLoggedIn = event => {
         this.setState({ isLoggedIn: event }, () => {
             return <Route to='/renderWindow' />;
@@ -37,27 +42,30 @@ class App extends React.Component {
         }
 
         return (
-            <Router>
-                <div className='home_link'>
-                    <Link
-                        className='link_text'
-                        to='/login'
-                        onClick={this.handleWhichMessage}>
-                        {this.state.whichMessage
-                            ? "Welcome, click to login"
-                            : "Please enter your credentials below"}
-                    </Link>
-                </div>
+            <div>
+                <Nav className='nav_bar' handleAllNav={this.handleAllNav} />
+                <Router>
+                    <div className='home_link'>
+                        <Link
+                            className='link_text'
+                            to='/login'
+                            onClick={this.handleWhichMessage}>
+                            {this.state.whichMessage
+                                ? "Welcome, click to login"
+                                : "Please enter your credentials below"}
+                        </Link>
+                    </div>
 
-                <Switch>
-                    <Route path='/login'>
-                        <LoginWindow handleLoggedIn={this.handleLoggedIn} />
-                    </Route>
-                    <Route path='/renderWindow'>
-                        <RenderWindow />
-                    </Route>
-                </Switch>
-            </Router>
+                    <Switch>
+                        <Route path='/login'>
+                            <LoginWindow handleLoggedIn={this.handleLoggedIn} />
+                        </Route>
+                        <Route path='/renderWindow'>
+                            <RenderWindow />
+                        </Route>
+                    </Switch>
+                </Router>
+            </div>
         );
     }
 }
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-function NavigationBar() {
+function NavigationBar(props) {
+    const answer = props.handleAllNav;
+    let Nav;
+    if (answer) {
+    }
+
     return (
-        <nav className="nav_bar">
+        <nav className='nav_bar'>
             <h3>Logo here</h3>
-            <ul className="nav_links">
+            <ul className='nav_links'>
                 <li>Home</li>
                 <li>Account</li>
                 <li>Shop</li>
